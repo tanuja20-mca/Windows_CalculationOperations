30ffb909feaabb89efb6660555af30d5ea7130c6
Dell <Dell@DESKTOP-RN29GV5>
2022-02-07 17:14:18 +0530

    Add project files.

---
+++ Calculation_1/calculation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculation_1
+{
+    public class calculation
+    {
+
+        public int addition(int a, int b)
+        {
+            int result = a + b;
+            return result;
+        }
+
+        public int subtraction(int a, int b)
+        {
+            int result = a - b;
+            return result;
+        }
+
+        public int multiplication(int a, int b)
+        {
+            int result = a * b;
+            return result;
+        }
+
+        public  int division(int a, int b)
+        {
+            int result = a / b;
+            return result;
+        }
+
+
+    }
+}
---
+++ Windows_Calculation/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Calculation_1;
+
+namespace Windows_Calculation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(textBox1.Text);
+            int b = Convert.ToInt32(textBox2.Text);
+
+            calculation cal = new calculation();
+            textBox3.Text = Convert.ToString(cal.addition(a, b));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(textBox1.Text);
+            int b = Convert.ToInt32(textBox2.Text);
+            calculation cal = new calculation();
+            textBox3.Text = Convert.ToString(cal.subtraction(a, b));
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(textBox1.Text);
+            int b = Convert.ToInt32(textBox2.Text);
+            calculation cal = new calculation();
+            textBox3.Text = Convert.ToString(cal.multiplication(a, b));
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(textBox1.Text);
+            int b = Convert.ToInt32(textBox2.Text);
+            calculation cal = new calculation();
+            textBox3.Text = Convert.ToString(cal.division(a, b));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
